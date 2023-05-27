823a88e6970db5826337d7a9b8df7a2e606f619d
Alejandro Gomez <alejandro@strike.me>
2023-05-27 17:56:18 +0200

    fix: add key

--- components/Text.tsx
+++ components/Text.tsx
@@ -2,10 +2,12 @@ import { Text } from "@chakra-ui/react";
 import Emoji from "@emoji/components/Emoji";
 
 function extractCustomEmoji(f, tags) {
-  return f.split(/:(\w+):/g).map((i) => {
+  return f.split(/:(\w+):/g).map((i, idx) => {
     const t = tags.find((a) => a[0] === "emoji" && a[1] === i);
     if (t) {
-      return <Emoji display="inline" boxSize={5} src={t[2]} name={t[0]} />;
+      return (
+        <Emoji key={idx} display="inline" boxSize={5} src={t[2]} name={t[0]} />
+      );
     } else {
       return i;
     }
